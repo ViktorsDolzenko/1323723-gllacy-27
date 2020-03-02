c23da1c8f7c35aa6b1470ed34dc1fbbcc8b31b7a
Viktors Dolzenko <vitja1234@gmail.com>
2020-03-02 21:29:48 +0200

    Личный проект: начинаем оформление декоративных элементов

---
+++ js/main.js
@@ -0,0 +1,53 @@
+var feedbtn = document.querySelector(".feedback-entry");
+var popupfeed = document.querySelector(".feedback-section");
+var close = popupfeed.querySelector(".feedback-close");
+var form = document.querySelector(".feedback");
+var name = popupfeed.querySelector("[name=username]");
+var email = popupfeed.querySelector("[name=useremail]")
+var isStorageSupport = true;
+var storage = "";
+var modal
+
+try {
+  storage = localStorage.getItem("name");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+
+
+feedbtn.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popupfeed.classList.add("modal-show");
+  if (storage) {
+    name.value = storage;
+  
+  
+
+  }
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popupfeed.classList.remove("modal-show");
+  popupfeed.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!name.value || !email.value) {
+    evt.preventDefault();
+    popupfeed.classList.remove("modal-error");
+    popupfeed.offsetWidth = popupfeed.offsetWidth
+    popupfeed.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("name", name.value)
+
+    }
+  }
+
+
+});
+
+
+
